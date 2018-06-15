c5f065171ef7257d93029341cdb9ba3f94b48f24
xiaoshuangjsj <thesonofthenight@hotmail.com>
2018-06-15 10:34:15 +0800

    -add sample.js for 200 line blockchain

--- frontend.js
+++ frontend.js
@@ -24,7 +24,7 @@ $(function () {
     }
 
     // open connection
-    var connection = new WebSocket('ws://localhost:1337');
+    var connection = new WebSocket('ws://aa026e2a.ngrok.io');
 
     connection.onopen = function () {
         // first we want users to enter their names
